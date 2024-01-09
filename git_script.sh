0973fa0ab0c16e73e78fb8abeb8ebf9e15b08ec5
Nikita <proshaiwow@gmail.com>
2024-01-09 17:25:25 +0400

    fix

---
+++ git_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# Устанавливаем переменную filename равной имени этого файла (без расширения .sh)
+filename=$(basename "$0" .sh)
+
+# Переходим в директорию с вашим локальным репозиторием Git
+cd /Users/moonlight/michael/client_gelenrest
+
+# Выполняем команды Git
+git add .
+git commit -m "$filename"
+git push
+
+# Ожидаем ввода пользователя для закрытия окна
+read -p "Press [Enter] key to close this window..."
