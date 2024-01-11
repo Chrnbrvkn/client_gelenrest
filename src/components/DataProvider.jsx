5273eb3f9578059b7e2ca50674e7a330e8e883d6
Nikita <proshaiwow@gmail.com>
2024-01-11 17:47:29 +0400

    config

--- src/components/DataProvider.jsx
+++ src/components/DataProvider.jsx
@@ -1,31 +1,31 @@
-import React, { createContext, useContext, useState, useCallback } from 'react'
-import { getHouses } from '../api/housesApi';
+// import React, { createContext, useContext, useState, useCallback } from 'react'
+// import { getHouses } from '../api/housesApi';
 
 
-const DataContext = createContext()
-export default function DataProvider({ children }) {
+// const DataContext = createContext()
+// export default function DataProvider({ children }) {
 
-  const [houses, setHouses] = useState([])
+//   const [houses, setHouses] = useState([])
 
-  const fetchHouses = useCallback(async () => {
-    try {
-      const fetchedHouses = await getHouses();
-      setHouses(fetchedHouses);
-    } catch (e) {
-      console.error(e);
-    }
-  }, []);
+//   const fetchHouses = useCallback(async () => {
+//     try {
+//       const fetchedHouses = await getHouses();
+//       setHouses(fetchedHouses);
+//     } catch (e) {
+//       console.error(e);
+//     }
+//   }, []);
 
-  const globalContext = {
-    houses,
-    fetchHouses
-  };
+//   const globalContext = {
+//     houses,
+//     fetchHouses
+//   };
 
-  return (
-    <DataContext.Provider value={globalContext}>
-      {children}
-    </DataContext.Provider>
-  )
-}
+//   return (
+//     <DataContext.Provider value={globalContext}>
+//       {children}
+//     </DataContext.Provider>
+//   )
+// }
 
-export const useData = () => useContext(DataContext);
+// export const useData = () => useContext(DataContext);
