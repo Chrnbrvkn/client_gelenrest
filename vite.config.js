1665e47890eaef6e18b0d1b8e755f9cd7e848a6a
Nikita <proshaiwow@gmail.com>
2024-01-11 07:32:33 +0400

    config

--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,14 @@ import fs from 'fs';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
 export default defineConfig({
   plugins: [react()],
-  server: {
+  server: isDevelopment ? {
     https: {
       key: fs.readFileSync('C:/Users/prosh/Desktop/localSSL/localhost-key.pem'),
       cert: fs.readFileSync('C:/Users/prosh/Desktop/localSSL/localhost.pem')
     }
-  }
+  } : {}
 });
