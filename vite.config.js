c3d9071cd6d98e27d14a4dc9e1d1c268f044c9ed
Nikita <proshaiwow@gmail.com>
2024-01-11 07:24:05 +0400

    config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import fs from 'fs';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    https: {
+      key: fs.readFileSync('C:/Users/prosh/Desktop/localSSL/localhost-key.pem'),
+      cert: fs.readFileSync('C:/Users/prosh/Desktop/localSSL/localhost.pem')
+    }
+  }
+});
