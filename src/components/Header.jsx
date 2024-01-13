f1b7c0c5198418179f6418458e9ef63cf12371a2
Nikita <proshaiwow@gmail.com>
2024-01-13 17:02:54 +0400

    contexts

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,25 +9,25 @@ function Header() {
                 <NavLink className='header_link' to="/login">Login</NavLink>
             </li> */}
             <li className="header_list-item">
-                <NavLink className='header_link' to="/admin">Admin</NavLink>
+                <NavLink className='header_link' to={`/admin`}>Admin</NavLink>
             </li>
             <div className="container">
                 <div className="header__top-content">
                     <ul className="header__menu-items">
                         <li className="header__menu-item">
-                            <NavLink className='header__menu-link' to="/">О нас</NavLink>
+                            <NavLink className='header__menu-link' to={`/`}>О нас</NavLink>
                         </li>
                         <li className="header__menu-item">
-                            <NavLink className='header__menu-link' to="/houses">Дома</NavLink>
+                            <NavLink className='header__menu-link' to={`/houses`}>Дома</NavLink>
                         </li>
                         <li className="header__menu-item">
-                            <NavLink className='header__menu-link' to="/apartments">Квартиры</NavLink>
+                            <NavLink className='header__menu-link' to={`/apartments`}>Квартиры</NavLink>
                         </li>
                         <li className="header__menu-item">
-                            <NavLink className='header__menu-link' to="/reservation">Бронирование</NavLink>
+                            <NavLink className='header__menu-link' to={`/reservation`}>Бронирование</NavLink>
                         </li>
                         <li className="header__menu-item">
-                            <a className='header__menu-link' href="">Отзывы</a>
+                            <NavLink className='header__menu-link' href="#">Отзывы</NavLink>
                         </li>
                     </ul>
                     <div className="header__contacts">
@@ -38,7 +38,37 @@ function Header() {
                             +7 (924)-212-23-77
                         </a>
                     </div>
+                </div>
 
+                <div className="header__top-content header__top-content--min">
+                    <div className="header__contacts">
+                        <a href="#" className="header__logo">
+                            <img src="/src/assets/images/icons/logo.png" alt="" />
+                        </a>
+                        <button href="tel:89242122377" className="header__contacts-btn">
+                            +7 (937) 667 20-21
+                        </button>
+                        <button className="menu__btn"></button>
+                    </div>
+
+
+                    <ul className="header__menu-items">
+                        <li className="header__menu-item">
+                            <NavLink className='header__menu-link' to={`/`}>О нас</NavLink>
+                        </li>
+                        <li className="header__menu-item">
+                            <NavLink className='header__menu-link' to={`/houses`}>Дома</NavLink>
+                        </li>
+                        <li className="header__menu-item">
+                            <NavLink className='header__menu-link' to={`/apartments`}>Квартиры</NavLink>
+                        </li>
+                        <li className="header__menu-item">
+                            <NavLink className='header__menu-link' to={`/reservation`}>Бронирование</NavLink>
+                        </li>
+                        <li className="header__menu-item">
+                            <a className='header__menu-link' href="#">Отзывы</a>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </header>
