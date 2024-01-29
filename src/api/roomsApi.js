79d5674ba7bee3de1fb6013aae96a4fd3dfb98ea
Nikita <proshaiwow@gmail.com>
2024-01-30 00:59:17 +0400

    fix

--- src/api/roomsApi.js
+++ src/api/roomsApi.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getAllRooms = async () => {
   try {
-    const response = await axios.get(`https://api.gelenrest.ru/api/rooms`)
+    const response = await axios.get(`https://api.gelenrest.ru/rooms`)
     return response.data
   } catch (e) {
     console.error(e);
@@ -11,7 +11,7 @@ export const getAllRooms = async () => {
 
 export const getRooms = async (houseId) => {
   try {
-    const response = await axios.get(`https://api.gelenrest.ru/api/rooms/${houseId}`)
+    const response = await axios.get(`https://api.gelenrest.ru/rooms/${houseId}`)
     return response.data
   } catch (e) {
     console.error(e)
@@ -20,7 +20,7 @@ export const getRooms = async (houseId) => {
 
 export const getRoom = async (roomId, houseId) => {
   try {
-    const response = await axios.get(`https://api.gelenrest.ru/api/rooms/${houseId}/${roomId}`)
+    const response = await axios.get(`https://api.gelenrest.ru/rooms/${houseId}/${roomId}`)
     return response.data
   } catch (e) {
     console.error(e);
@@ -29,7 +29,7 @@ export const getRoom = async (roomId, houseId) => {
 export const createRoom = async (houseId, room) => {
   try {
     const response = await axios.post(
-      `https://api.gelenrest.ru/api/rooms/${houseId}`,
+      `https://api.gelenrest.ru/rooms/${houseId}`,
       room, { headers: { 'Content-Type': 'multipart/form-data' } }
     )
     return response.data
@@ -40,7 +40,7 @@ export const createRoom = async (houseId, room) => {
 
 export const updateRoom = async (houseId, roomId, room) => {
   try {
-    const response = await axios.patch(`https://api.gelenrest.ru/api/rooms/${houseId}/${roomId}`, room
+    const response = await axios.patch(`https://api.gelenrest.ru/rooms/${houseId}/${roomId}`, room
     )
     return response.data
   } catch (e) {
@@ -50,7 +50,7 @@ export const updateRoom = async (houseId, roomId, room) => {
 
 export const deleteRoom = async (houseId, roomId) => {
   try {
-    await axios.delete(`https://api.gelenrest.ru/api/rooms/${houseId}/${roomId}`)
+    await axios.delete(`https://api.gelenrest.ru/rooms/${houseId}/${roomId}`)
     return console.log(`Room with ID: ${roomId} был удалён!`)
   } catch (e) {
     console.error(e);
@@ -58,7 +58,7 @@ export const deleteRoom = async (houseId, roomId) => {
 }
 export const getRoomAllImages = async () => {
   try {
-    const response = await axios.get(`https://api.gelenrest.ru/api/room/pictures`)
+    const response = await axios.get(`https://api.gelenrest.ru/room/pictures`)
     return response.data
   } catch (e) {
     console.error(e);
@@ -66,7 +66,7 @@ export const getRoomAllImages = async () => {
 }
 export const getRoomImages = async (roomId) => {
   try {
-    const response = await axios.get(`https://api.gelenrest.ru/api/room/${roomId}/pictures`)
+    const response = await axios.get(`https://api.gelenrest.ru/room/${roomId}/pictures`)
     return response.data
   } catch (e) {
     console.error(e);
@@ -75,7 +75,7 @@ export const getRoomImages = async (roomId) => {
 
 export const getRoomOneImage = async (roomId, imageId) => {
   try {
-    const response = await axios.get(`https://api.gelenrest.ru/api/room/${roomId}/pictures/${imageId}`)
+    const response = await axios.get(`https://api.gelenrest.ru/room/${roomId}/pictures/${imageId}`)
     return response.data
   } catch (e) {
     console.error(e);
@@ -89,7 +89,7 @@ export const uploadRoomPictures = async (pictures, roomId) => {
       formData.append(`roomsPictures`, picture)
     })
     formData.append('roomId', roomId)
-    await axios.post(`https://api.gelenrest.ru/api/room/${roomId}/pictures`, formData)
+    await axios.post(`https://api.gelenrest.ru/room/${roomId}/pictures`, formData)
   } catch (e) {
     console.error(e);
   }
@@ -97,7 +97,7 @@ export const uploadRoomPictures = async (pictures, roomId) => {
 
 export const deleteRoomPicture = async (roomId, imageId) => {
   try {
-    await axios.delete(`https://api.gelenrest.ru/api/room/${roomId}/pictures/${imageId}`)
+    await axios.delete(`https://api.gelenrest.ru/room/${roomId}/pictures/${imageId}`)
     return console.log(`Picture with ID: ${imageId} was deleted`);
   } catch (e) {
     console.error(e);
